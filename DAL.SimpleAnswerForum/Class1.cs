44132f06c82c8a17fb54e0aadebb3ee95b3922fc
Shadman Kudchikar <shadmanssk@yahoo.in>
2019-04-27 20:57:26 +0530

    remove unwanted folders

--- DAL.SimpleAnswerForum/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DAL.SimpleAnswerForum
-{
-    public class Class1
-    {
-    }
-}
--- UserManagement.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace UserManagement.Core
-{
-    public class Class1
-    {
-    }
-}
